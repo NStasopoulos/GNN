0dd64b19130287374c9a8b766fe7c6083eb50290
npanagi <npanagi@uclan.ac.uk>
2018-11-29 16:18:32 +0200

    Added javascript to th-api.js

--- th-api.js
+++ th-api.js
@@ -1,4 +1,23 @@
-const UUID = "ag9nfmNvZGVjeXBydXNvcmdyGQsSDFRyZWFzdXJlSHVudBiAgICAvKGCCgw"
+// function Answer() {
+//     var xhttp = new XMLHttpRequest();
+//
+//     xhttp.onreadystatechange = function () {
+//         if (this.readyState === 4 && this.status === 200) {
+//
+//             object = JSON.parse(this.responseText);
+//
+//             //code here
+//
+//         }
+//         else {
+//             //TODO If response not received (error).
+//         }
+//
+//     };
+//
+//     xhttp.open("GET", "https://codecyprus.org/th/api/question?session=" + document.cookie , true);
+//     xhttp.send();
+// }
 
 function list() {
     xhttp = new XMLHttpRequest();
@@ -15,7 +34,7 @@ function list() {
                 var newItem = document.createElement("li");
                 var linkItem = document.createElement("a");
                 linkItem.innerHTML = object.treasureHunts[i].name;
-                linkItem.href = "login.html";
+                linkItem.href = "login.html?uuid=" + object.treasureHunts[i].uuid;
                 newItem.appendChild(linkItem);
                 challangesList.appendChild(newItem);
 
@@ -33,6 +52,10 @@ function start() {
     document.getElementById("SubmitButton").onclick = function SaveDetails() {
         var Username = document.getElementById("loginname");
         var Aname = document.getElementById("loginappname");
+        var url = new URL(window.location.href);
+        var uuid = url.searchParams.get('uuid');
+        console.log(uuid);
+
         var xhttp = new XMLHttpRequest();
 
         xhttp.onreadystatechange = function () {
@@ -51,13 +74,12 @@ function start() {
             }
         };
 
-        xhttp.open("GET", "https://codecyprus.org/th/api/start?player=" + Username.value + "&app=" + Aname.value + "&treasure-hunt-id=" + UUID, true);
+        xhttp.open("GET", "https://codecyprus.org/th/api/start?player=" + Username.value + "&app=" + Aname.value + "&treasure-hunt-id=" + uuid, true);
         xhttp.send();
     };
 }
 
 function questions() {
-    console.log("en o shistos");
     var xhttp = new XMLHttpRequest();
 
     xhttp.onreadystatechange = function () {
@@ -65,12 +87,44 @@ function questions() {
 
             object = JSON.parse(this.responseText);
 
-            if (object.requiresLocation === true)
-                getLocation();
-
-            console.log(object);
+            //console.log(object.questionType);
             var qText = document.getElementById("qText");
-            qText.innerHTML = object.questionText;
+            qText.innerHTML = "<p>" + object.questionText +"</p>";
+
+            if (object.questionType === "BOOLEAN") {
+                var qType = document.getElementById("qType");
+                qType.innerHTML = "True <input class='radio' type='radio' name='answer' value='True'>" +
+                    "False <input class='radio' type='radio' name='answer' value='False'>" +
+                    "<input class='submitBut' type='button' name='submit' value='Submit' onclick='Answer()'>" +
+                    "<input class='skipBut' type='button' name='Skip' value='Skip Question' onclick='Skip()'>"
+            }
+            else if (object.questionType === "INTEGER") {
+                var qType = document.getElementById("qType");
+                qType.innerHTML = "Your answer: <input class='integer' type='number' name='answer'>" +
+                    "<input class='submitBut' type='button' name='submit' value='Submit' onclick='Answer()'>" +
+                    "<input class='skipBut' type='button' name='Skip' value='Skip Question' onclick='Skip()'>"
+            }
+            else if (object.questionType === "NUMERIC") {
+                var qType = document.getElementById("qType");
+                qType.innerHTML = "Your answer: <input class='integer' type='number' name='answer'>" +
+                    "<input class='submitBut' type='button' name='submit' value='Submit' onclick='Answer()'>" +
+                    "<input class='skipBut' type='button' name='Skip' value='Skip Question' onclick='Skip()'>"
+            }
+            else if (object.questionType === "MCQ") {
+                var qType = document.getElementById("qType");
+                qType.innerHTML = "(A) <input class='radio' type='radio' name='answer' value='A'>" +
+                    "(B) <input class='radio' type='radio' name='answer' value='B'>" +
+                    "(C) <input class='radio' type='radio' name='answer' value='C'>" +
+                    "(D) <input class='radio' type='radio' name='answer' value='D'>" +
+                    "<input class='submitBut' type='button' name='submit' value='Submit' onclick='Answer()'>" +
+                    "<input class='skipBut' type='button' name='Skip' value='Skip Question' onclick='Skip()'>"
+            }
+            else if (object.questionType === "TEXT") {
+                var qType = document.getElementById("qType");
+                qType.innerHTML = "Your answer: <input class='text' type='text' name='answer'>" +
+                    "<input class='submitBut' type='button' name='submit' value='Submit' onclick='Answer(object)'>" +
+                    "<input class='skipBut' type='button' name='Skip' value='Skip Question' onclick='Skip()'>"
+            }
 
         }
         else {
@@ -79,5 +133,44 @@ function questions() {
     };
 
     xhttp.open("GET", "https://codecyprus.org/th/api/question?session=" + document.cookie , true);
+    xhttp.send();
+}
+
+function Answer(object) {
+    console.log(object.questionType);
+    if (object.questionType === "TEXT") {
+        var answer = document.getElementsByClassName("text");
+        var ans = answer[0].value;
+    }
+    else if (object.questionType === "BOOLEAN" || object.questionType === "MCQ") {
+        var answer = document.getElementsByClassName("radio");
+        var ans = answer[0].value;
+        for (let i = 0; i < answer[0].length; i++) {
+            if (ansForm[0].elements[i].checked)
+                ans = answer[0].elements[i].value;
+        }
+    }
+    else if (object.questionType === "NUMERIC" || object.questionType === "INTEGER") {
+        var answer = document.getElementsByClassName("integer");
+        var ans = answer[0].value;
+    }
+    console.log(ans)
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+
+            object = JSON.parse(this.responseText);
+
+            //code here
+
+        }
+        else {
+            //TODO If response not received (error).
+        }
+
+};
+
+    xhttp.open("GET", "https://codecyprus.org/th/api/answer?session=" + document.cookie + "&answer=" + ans.value , true);
     xhttp.send();
 }
